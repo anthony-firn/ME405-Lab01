b57ddc19779b0931ccc5078181d2b9a931cb8aba
anthony4tner <anthony4tner2@gmail.com>
2019-04-16 15:56:28 -0700

    p controll

--- controller.py
+++ controller.py
@@ -13,12 +13,26 @@ class Controller:
     
     
     def __init__(self, gain, setpoint):
-        self.gain= gain
+        ''' Initializes controll loop to parameters'''
 
+        self.kp = kp
         self.setpoint= setpoint
         
         
-    def 
+    def calculate(self, position):
+        ''' Computes the controll response'''
+        
+        # compute error and pwm output
+        self._error = self.setpoint - position
+        self._pwm = self.kp * self._error
+
+        # saturation
+        if (self._pwm > 100) :
+            self._pwm = 100
+        elif (self._pwm < -100) :
+            self._pwm = -100
+
+        return self._pwm
         
         
     def set_gain(self, setting_gain):
