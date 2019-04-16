63f3c9cb1795778d28e2cb77914753232fb821a1
clmendez <43482677+clmendez@users.noreply.github.com>
2019-04-16 16:56:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import controller
 import encoder
 import motor
 import pyb
+import utime
 
 def main():
     control = controller.Controller(1, 1000)
     motor1 = motor.MotorDriver()
     encoder1 = encoder.Encoder(pyb.Pin.board.PB6, pyb.Pin.board.PB7, pyb.Timer(4))
-    
+    control_duration = 100
+
     while True:
-        pwm = control.calculate(encoder1.get_position())
-        motor1.set_duty_cycle(pwm)
+        key = input()
+        if key == "\r":
+            start_time = utime.ticks_ms()
+            while (start_time + control_duration) < utime.ticks_ms():
+                pwm = control.calculate(encoder1.get_position())
+                motor1.set_duty_cycle(pwm)
+                key = None
         
         
         
