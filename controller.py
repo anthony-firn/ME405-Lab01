8e1ff313bb233a137ceaef30b4f97c1b40f9bc0d
clmendez <43482677+clmendez@users.noreply.github.com>
2019-04-16 15:37:07 -0700

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 15:31:05 2019
+
+@author: claud
+"""
+
+import pyb
+
+
+
+class Controller:
+    
+    
+    def __init__(self, gain, setpoint):
+        self.= pyb.Pin (pin1, pyb.Pin.IN)
+
+        self.= pyb.Pin (pin2, pyb.Pin.IN)
+    
+        
+    
+    
