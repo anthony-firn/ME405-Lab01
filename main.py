56ac897913ac4d7782bc11e7c6fc29417a666373
clmendez <43482677+clmendez@users.noreply.github.com>
2019-04-16 17:24:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     control = controller.Controller(1, 1000)
     motor1 = motor.MotorDriver()
     encoder1 = encoder.Encoder(pyb.Pin.board.PB6, pyb.Pin.board.PB7, pyb.Timer(4))
-    control_duration = 100
+    control_duration = 1000
 
     while True:
         key = input("press a")
@@ -18,7 +18,8 @@ def main():
             start_time = utime.ticks_ms()
             print("start" + str(start_time))
             while (start_time + control_duration) > utime.ticks_ms():
-                print( "current time" + str(utime.ticks_ms))
+                current_time = utime.ticks_ms
+                print( "current time" + str(current_time))
                 pwm = control.calculate(encoder1.get_position())
                 motor1.set_duty_cycle(pwm)
                 key = None
